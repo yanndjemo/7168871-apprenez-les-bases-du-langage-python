154228cb1bbe9fd7da6b8c0b9d5cb3fc7408e1e6
yanndjemo <114511830+yanndjemo@users.noreply.github.com>
2024-12-11 08:54:06 +0100

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,10 @@
-# Écrivez votre code ici !
+nom = "yann" 
+age = 24
+taille = 1.70
+est_etudiant = True
+print(f" je m'appelle {yann} j'ai {24} ans je mesure {taille} metre ")
+print("est_etudiant")
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
